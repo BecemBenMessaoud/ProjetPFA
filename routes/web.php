90226f3d9366d8212abc9715fbcf32fb79aebef6
vagrant <vagrant@homestead>
2022-02-16 18:26:52 +0000

    delete old files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\RegionController;
 use App\Http\Controllers\User\ArticleController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,28 +14,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['prefix' => 'admin'], function () {
-    Route::group(['prefix' => 'regions'], function () {
-        Route::get('/', ['App\Http\Controllers\Admin\RegionController', 'index']);
-        Route::get('/create', ['App\Http\Controllers\Admin\RegionController', 'create']);
-        Route::post('/store', ['App\Http\Controllers\Admin\RegionController', 'store']);
-        Route::get('/edit/{region_id}', ['App\Http\Controllers\Admin\RegionController', 'edit']);
-        Route::post('/update/{region_id}', ['App\Http\Controllers\Admin\RegionController', 'update']);
-        Route::delete('/delete/{region_id}', ['App\Http\Controllers\Admin\RegionController', 'delete']);
+
+
+Route::prefix('admin')->group(function () {
+    Route::prefix('regions')->controller(RegionController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/create', 'create');
+        Route::post('/store', 'store');
+        Route::get('/edit/{region_id}', 'edit');
+        Route::post('/update/{region_id}', 'update');
+        Route::delete('/delete/{region_id}', 'delete');
     });
-});
-
-
-Route::group(['prefix' => 'user'], function () {
-
-    Route::group(['prefix' => 'article'], function () {
-        Route::post('/', [ArticleController::class, 'create']);
-        Route::delete('/{article_id}', [ArticleController::class, 'delete']);
-    });
-
-    Route::group(['prefix' => 'demand'], function () {
-        // demand routes
-    });
-
-
 });
